fb4db450fa8bc2df093ae2f6ab3827941d7d68ae
arnob550 <abrarshahrierarnab@gmail.com>
2025-10-03 13:37:29 +0600

    UPDATE cOMMIT

---
+++ Celcius to Farenhaiet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int c,f;
+    printf("Enter temperature in Celsius: ");
+    scanf("%d",&c);
+    f=(c*9/5)+32;
+    printf("Temperature in Fahrenheit: %d\n",f);
+    return 0;
+}
